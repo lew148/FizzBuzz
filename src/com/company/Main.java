d88a2ee2c3a0ec72f80f8c12c22f7615f5a5d655
Lewis Reid <lewis.reid148@gmail.com>
2018-09-11 11:17:39 +0100

    Update 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,7 +5,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-        for (Integer i = 1; i <= 150; i++) {
+        for (Integer i = 1; i <= 300; i++) {
             String result = "";
 
             if (i % 3 == 0) {
